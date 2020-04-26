b3bc4a02b55602b9bd24fbfda36c95ad9c03e653
markorose <48208980+markorose@users.noreply.github.com>
2020-04-26 14:03:21 +0200

    Update box_bps.js

--- box/box_bps.js
+++ box/box_bps.js
@@ -1,4 +1,5 @@
-(function()  {
+(function()  
+ {
 	let template = document.createElement("template");
 	template.innerHTML = `
 		<form id="form">
@@ -17,17 +18,17 @@
 		:host {
 			display: block;
 			padding: 1em 1em 1em 1em;
-		}
+		       }
 		</style>
 	`;
 
-	class BoxBps extends HTMLElement {
+	class GaugeBps extends HTMLElement {
 		constructor() {
 			super();
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
 			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-		}
+		               }
 
 		_submit(e) {
 			e.preventDefault();
@@ -35,18 +36,18 @@
 					detail: {
 						properties: {
 							color: this.color
-						}
-					}
-			}));
-		}
+						             }
+					        }
+			                                                          }));
+		              }
 
 		set color(newColor) {
 			this._shadowRoot.getElementById("bps_color").value = newColor;
-		}
+		                    }
 
 		get color() {
 			return this._shadowRoot.getElementById("bps_color").value;
-		}
+		             }
 	}
 
 	customElements.define("com-sac-gauge-bps", GaugeBps);
